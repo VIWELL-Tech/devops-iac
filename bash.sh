a6f18bde27cd0477dff6b6f917ab7d0ed8f0fe45
Abdulrahman Hassan <abdulrahman.hassan@slashtec.com>
2023-06-06 16:43:48 +0300

    fix

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the file to be copied
-source_file="./hpa.yaml"
+file_to_delete="secret-dev.yml"
 
 # Specify the root directory to search in
 root_directory="."
@@ -10,6 +10,7 @@ root_directory="."
 target_folder="templates"
 
 # Search for the target folder and copy the file into it
-find "$root_directory" -type d -name "$target_folder" -exec cp "$source_file" {}/ \;
+# find "$root_directory" -type d -name "$target_folder" -exec cp "$source_file" {}/ \;
+find "$root_directory" -type d -name "$target_folder" -exec rm -f {}/"$file_to_delete" \;
 
 echo "File copied to target folders within $root_directory"
