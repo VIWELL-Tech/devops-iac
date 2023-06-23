ab887bde084e2e14d865413859a82ed5f79da2c0
saied <muhamed.abdelhaliem@gmail.com>
2023-06-23 19:05:44 +0300

    update

--- AWS/viwell-uae-prod/3_mongo-atlas/variables.tf
+++ AWS/viwell-uae-prod/3_mongo-atlas/variables.tf
@@ -1,28 +1,33 @@
 # Atlas Organization ID 
 variable "atlas_org_id" {
   type        = string
+  default = "6318a3cb5d9b7743898c33d8"
   description = "Atlas Organization ID"
 }
 # Atlas Project Name
 variable "atlas_project_name" {
   type        = string
   description = "Atlas Project Name"
+  default = "PROJECT 0"
 }
 
 # Atlas Project Environment
 variable "environment" {
   type        = string
+  default = "production"
   description = "The environment to be built"
 }
 
 # Cluster Instance Size Name 
 variable "cluster_instance_size_name" {
   type        = string
+  default = "m40"
   description = "Cluster instance size name"
 }
 
 variable "cluster_username" {
   type        = string
+  default = "admin"
   description = "admin username"
 }
 
@@ -34,18 +39,21 @@ variable "cluster_password" {
 # Cloud Provider to Host Atlas Cluster
 variable "cloud_provider" {
   type        = string
+  default = "aws"
   description = "AWS or GCP or Azure"
 }
 
 # Atlas Region
 variable "atlas_region" {
   type        = string
+  default = "me-central-1"
   description = "Atlas region where resources will be created"
 }
 
 # MongoDB Version 
 variable "mongodb_version" {
   type        = string
+  default = "5.0.18"
   description = "MongoDB Version"
 }
 
@@ -58,5 +66,6 @@ variable "ip_address" {
 # AWS Region
 variable "aws_region" {
   type = string
+  default = "me-central-1"
   description = "AWS Region"
 }
