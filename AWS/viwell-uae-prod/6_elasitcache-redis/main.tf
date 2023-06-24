87626ae9de35104ca0f5078f1c1bef3bb580657b
saied <muhamed.abdelhaliem@gmail.com>
2023-06-24 21:53:02 +0300

    add elastic cahe

--- AWS/viwell-uae-prod/6_elasitcache-redis/main.tf
+++ AWS/viwell-uae-prod/6_elasitcache-redis/main.tf
@@ -1,80 +1,29 @@
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_security_group" "additional" {
-  name_prefix = "msk-sg"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }  
-
-  ingress {
+module "viwell-prod-cluster" {
+  source = "git::git@github.com:VIWELL-Tech/devops-terraform-modules.git//aws-elasticache-redis?ref=main"
+  apply_immediately = "true"
+  auto_minor_version_upgrade = "true"
+  name                       = "viwell-prod-cluster"
+  cluster_size               = "1"
+  automatic_failover_enabled  = "false"
+  multi_az_enabled            = "false"
+  redis_node_type             = "cache.t2.small"
+  family                      = "redis6.x"
+  redis_version               = "6.x"
+  at_rest_encryption_enabled  = "false"
+  transit_encryption_enabled  = "false"
+  cluster_mode_enabled        = "false"
+  vpc_id                      = "vpc-0c708b8993066e7fe"
+  subnet_ids                  = ["subnet-0e227f493a97404ac","subnet-02230bb50ebc733f1","subnet-07bcff630d9622561"]
+  subnet_group_name           = "viwell-prod-cluster"
+  availability_zones          = ["me-central-1c"]
+  redis_secret_name           = "viwell_prod_cluster"
+  redis_parameter_group_name  = "viwell-prod-cluster-redis6-pg"
+  redis_ingress_rules = [{
     from_port = 6379
     to_port   = 6379
     protocol  = "tcp"
-    cidr_blocks = [
-      data.terraform_remote_state.vpc.outputs.vpc_cidr_block
-    ]
- }
-
-  tags = {
-    Name = "for redis"
+    cidr_blocks = "10.50.0.0/16"
+    description = "allow access to Redis from VPC "
   }
-}
-
-
-module "Redis" {
-  source  = "git::git@github.com:slashtec-code/terraform-modules.git//aws-elasticache-redis?ref=main"
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
-  availability_zones= data.terraform_remote_state.eks.outputs.worker_nodes_az
-  subnet_group_name= "development"
-  redis_parameter_group_name= "custom-redis6"
-  apply_immediately  = "true"
-  auto_minor_version_upgrade = "true"
-  ## redis_auth_token = null
-  name = "viwell-prod"
-  cluster_size = "2"
-  automatic_failover_enabled = "true"
-  multi_az_enabled   = "false"
-  redis_node_type = "cache.t3.medium"
-  redis_port = "6379"
-  family = "redis6.x"
-  redis_version = "6.x"
-  at_rest_encryption_enabled = "false"
-  transit_encryption_enabled = "false"
-  cluster_mode_enabled = "false"
-  cluster_mode_replicas_per_node_group = "0"
-  cluster_mode_num_node_groups = "0"
-  redis_maintenance_window = "fri:08:00-fri:09:00"
-  redis_snapshot_window = "06:30-07:30"
-  redis_snapshot_retention_limit = "0"
-  tags = {
-        Owner       = "DevOps Team"
-        Environment = "Development"
-  }
-}
-
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "dr-test-viwell"
-    key    = "infrastructure/prod/vpc.tfstate"
-    region = "us-west-2"
-  }
-}
-
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket = "dr-test-viwell"
-    key    = "infrastructure/prod/eks.tfstate"
-    region = "us-west-2"
-  }
+  ]
 }
--- AWS/viwell-uae-prod/6_elasitcache-redis/remote-state.tf
+++ AWS/viwell-uae-prod/6_elasitcache-redis/remote-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dr-test-viwell"
-    key    = "infrastructure/prod/redis.tfstate"
-    region = "us-west-2"
+    bucket         = "viwell-prod-infra"
+    key            = "viwell/prod-infra/elastic-cache/redis.tfstate"
+    region         = "me-central-1"
   }
 }
