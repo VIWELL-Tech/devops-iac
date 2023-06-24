6d4317d856f40adf107343189df83c2ca7e43103
saied <muhamed.abdelhaliem@gmail.com>
2023-06-25 00:06:13 +0300

    update

--- AWS/viwell-uae-prod/5_amazon-mq/variables.tf
+++ AWS/viwell-uae-prod/5_amazon-mq/variables.tf
@@ -6,24 +6,20 @@ variable "region" {
 
 variable "mq_admin_user" {
   type        = string
-  default     = null
   description = "Admin username"
 }
 
 variable "mq_admin_password" {
   type        = string
-  default     = null
   description = "Admin password"
 }
 
 variable "mq_application_user" {
   type        = string
-  default     = null
   description = "Application username"
 }
 
 variable "mq_application_password" {
   type        = string
-  default     = null
   description = "Application password"
 }
