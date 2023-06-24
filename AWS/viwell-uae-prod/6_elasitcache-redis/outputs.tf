0a3c0e87235fd9868e01517da7aaaa0a66d87b73
saied <muhamed.abdelhaliem@gmail.com>
2023-06-24 21:59:05 +0300

    update

--- AWS/viwell-uae-prod/6_elasitcache-redis/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = module.Redis.id
-  description = "Redis cluster id."
-}
-
-output "redis_endpoint" {
-  value       = module.Redis.endpoint
-  description = "Redis endpoint address."
-}
