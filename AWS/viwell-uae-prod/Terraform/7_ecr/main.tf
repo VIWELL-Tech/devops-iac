e1363af41d6ff8d38a7c09f04a2e35b899972004
saied <muhamed.abdelhaliem@gmail.com>
2023-06-27 00:34:21 +0300

    update

--- AWS/viwell-uae-prod/Terraform/7_ecr/main.tf
+++ AWS/viwell-uae-prod/Terraform/7_ecr/main.tf
@@ -9,5 +9,5 @@ module "ecr" {
   stage        = "prod"
   name         = "app"
   use_fullname = false
-  image_names  = ["assessment-v2","api-gateway","activity", "assessment", "gateway", "auth", "avatar", "booking", "category", "checkins", "content", "email", "favorite", "logger", "loyalty", "media", "notification", "organization", "personalisation", "pillar", "progress", "push-notification", "rbac", "report", "review", "reward", "schedular", "sms", "socket", "tag", "trigger"]
+  image_names  = ["viwell-seed","assessment-v2","api-gateway","activity", "assessment", "gateway", "auth", "avatar", "booking", "category", "checkins", "content", "email", "favorite", "logger", "loyalty", "media", "notification", "organization", "personalisation", "pillar", "progress", "push-notification", "rbac", "report", "review", "reward", "schedular", "sms", "socket", "tag", "trigger"]
 }
