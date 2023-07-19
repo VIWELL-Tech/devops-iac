ce5eb4148a6e50a9391cd33fa02c30a3ea51b027
saied <muhamed.abdelhaliem@gmail.com>
2023-07-19 18:04:38 +0300

    remove replication rule and enable ACL

--- AWS/viwell-uae-prod/Terraform/9_s3_buckets/production-viwell-media/main.tf
+++ AWS/viwell-uae-prod/Terraform/9_s3_buckets/production-viwell-media/main.tf
@@ -1,55 +1,4 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_iam_role" "replication" {
-  name = "tf-replication-role"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "s3.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "replication" {
-  name        = "tf-replication-policy"
-  description = "Policy for allowing S3 bucket replication"
-  policy      = data.aws_iam_policy_document.replication.json
-}
-
-data "aws_iam_policy_document" "replication" {
-  statement {
-    actions = [
-      "s3:GetBucketVersioning",
-      "s3:GetObjectVersionForReplication",
-      "s3:GetObjectVersionTagging",
-      "s3:ListBucket",
-      "s3:ReplicateDelete",
-      "s3:ReplicateObject",
-      "s3:ReplicateTags",
-    ]
-
-    resources = [
-      "arn:aws:s3:::image-resize-prod-814880204573-us-east-1",
-      "arn:aws:s3:::image-resize-prod-814880204573-us-east-1/*",
-    ]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "replication" {
-  role       = aws_iam_role.replication.name
-  policy_arn = aws_iam_policy.replication.arn
-}
-
-provider "aws" {
   alias  = "me_central"
   region = "me-central-1"
 }
@@ -58,32 +7,81 @@ resource "aws_s3_bucket" "destination_bucket" {
   provider = aws.me_central
 
   bucket = "production-viwell-media"
+  acl    = "public-read"  # tmp for now to make it compatiable with current communication from the code
 
   versioning {
     enabled = true
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "replication" {
-  bucket = "image-resize-prod-814880204573-us-east-1"
-  role   = aws_iam_role.replication.arn
+resource "aws_s3_bucket_cors" "bucket_cors" {
+  bucket = aws_s3_bucket.destination_bucket.bucket
 
-  rule {
-    id     = "tf-replication-rule"
-    status = "Enabled"
-    priority = 1
-  
-    filter {
-      prefix = "prod/"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "DELETE", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid       = "AllowCloudFrontServicePrincipal"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::production-viwell-media/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
-    delete_marker_replication {
-      status = "Enabled"
-    }
 
-
-    destination {
-      bucket        = aws_s3_bucket.destination_bucket.arn
-      storage_class = "STANDARD"
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::814880204573:distribution/E23P1XMVVLXPQX"]
     }
   }
+
+  statement {
+    sid       = "4"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::production-viwell-media/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E3U1MAYPBREW37"]
+    }
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::production-viwell-media/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::814880204573:role/media-resize-images-role"]
+    }
+  }
+
+  statement {
+    sid       = "8"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::production-viwell-media/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2K1N8DYNQKMG9"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.destination_bucket.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
 }
